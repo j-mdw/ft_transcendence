15fabefe07a451ebf8c055963ea22d06e32f6768
Florianne COUDERT <coudert.florianne.69@gmail.com>
2021-12-07 14:55:23 +0100

    qr code finally working thanks to Enzo

--- front/components/2FactorAuthentification.vue
+++ front/components/2FactorAuthentification.vue
@@ -8,16 +8,18 @@
         inset
         @change="activate2fa"
     >fa </v-switch>
-  <h1 v-if="qrCode">Vue is awesome!</h1>
-    <v-card-text class="text-center" style="padding: 24px !important">
+  <h1 v-if="test">Vue is awesome!</h1>
+    <!-- <v-card-text class="text-center" style="padding: 24px !important">
             <qrcode :value="qrCode" :options="{ width: 400 }" />
-          </v-card-text>
+          </v-card-text> -->
+    <v-img :src=qrCode> </v-img>
   </div>
 </template>
  
 <script lang="ts">
 import axios from 'axios'
 import Vue from 'vue'
+import { config } from 'vuex-module-decorators';
 import { authenticationStore }  from '~/store'
 export default Vue.extend({
     data: () => ({
@@ -28,7 +30,9 @@ export default Vue.extend({
     }),
     async mounted() {
       this.user = await this.$axios.$get("user/me", {withCredentials: true});
-      this.qrCode = await this.$axios.$post(`2fa/generate`, this.user, { withCredentials: true});
+      this.qrCode = "data:text/plain;base64," + await this.$axios.$post(`2fa/generate`, this.user,{ responseType: 'arraybuffer', withCredentials: true})
+      .then(response => Buffer.from(response, 'binary').toString('base64'))
+
       console.log("qrCode");
       console.log(this.qrCode);
 
@@ -37,9 +41,10 @@ export default Vue.extend({
 
       async activate2fa()
       {
-        
-        this.switch ? false : true;
+        console.log("coucou")
+        // this.switch ? false : true;
       },
+
     }
 
     
