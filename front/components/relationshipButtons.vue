23acda25a56a70336b8dc1e5af2f762c464cbdf6
Florianne Coudert <coudert.florianne.69@gmail.com>
2021-12-22 16:57:15 +0100

    relationship seens to work

--- front/components/relationshipButtons.vue
+++ front/components/relationshipButtons.vue
@@ -1,32 +1,47 @@
 <template>
     <div>
-          <div  v-if="myrelation.type == 1">
+          <!-- none -->
+          <div  v-if="myrelation.type == 0">
               <v-btn color="#f5cac3" class="mt-6" @click="askFriends()">
                 add to my friends
               <v-icon color="#395c6b" right>fa-user-plus</v-icon>    
             </v-btn>
-            <!-- send message  -->
           </div>
-          <div  v-if="status == 1">
+          <!-- invitation sent -->
+          <div  v-if="myrelation.type == 1">
+              <v-btn color="#f5cac3" class="mt-6" @click="deleteFriends()">
+                detete invitation
+              <v-icon color="#395c6b" right>fa-user-times</v-icon>    
+            </v-btn>
+          </div>
+           <!-- invitation received -->
+          <div  v-if="myrelation.type == 2">
+              <v-btn color="#f5cac3" class="mt-6" @click="becomeFriends()">
+                invitation received
+              <v-icon color="#395c6b" right>fa-user-times</v-icon>    
+            </v-btn>
+          </div>
+           <!-- we are friends -->
+          <div  v-if="myrelation.type == 3">
             <v-btn color="#f5cac3" v-bind="attrs" v-on="on" class="mt-6" >
                 send a message
               <v-icon color="#395c6b" right>fa-comment-alt</v-icon>    
             </v-btn>
             <v-row justify="center" align="center" class="mt-5">
-            <v-btn color="#f5cac3" v-bind="attrs" v-on="on" class="mt-6" >
+            <v-btn color="#f5cac3" v-bind="attrs" v-on="on" class="mt-6" @click="deleteFriends()">
                 Unfriend
               <v-icon color="#395c6b" right>fa-user-minus</v-icon>    
             </v-btn>
             </v-row>
-            <v-btn color="#f5cac3" v-bind="attrs" v-on="on" class="mt-6" >
+            <v-btn color="#f5cac3" v-bind="attrs" v-on="on" class="mt-6" @click="blockFriends()">
                 block this user
               <v-icon color="#395c6b" right>fa-user-times</v-icon>    
             </v-btn>
-            <!-- send message  -->
           </div>
-          <div v-if="status == 2">
-              <v-btn color="#EDEDED" v-bind="attrs" v-on="on" class="mt-6" >
-                you have been blocked
+           <!-- blocked -->
+          <div v-if="myrelation.type == 4">
+              <v-btn color="#EDEDED" v-bind="attrs" v-on="on" class="mt-6" @click="deleteFriends()">
+                you have blocked this user
               <v-icon color="#395c6b" right>fa-ban</v-icon>    
             </v-btn>
           </div>
@@ -68,13 +83,25 @@ export default Vue.extend({
         async askFriends()
         {
             await this.$axios.$post(`relationships/${this.userId}`, {type: RelationshipType.sent}, {withCredentials: true}).then((res) => {
-                console.log("we are friends");
+                console.log("ask to become friend");
             });
         },
         async becomeFriends()
         {
-            await this.$axios.$post(`relationships/${this.userId}`, {type: RelationshipType.sent}, {withCredentials: true}).then((res) => {
+            await this.$axios.$post(`relationships/${this.userId}`, {type: RelationshipType.friend}, {withCredentials: true}).then((res) => {
                 console.log("we are friends");
+            });
+        },
+        async deleteFriends()
+        {
+            await this.$axios.$delete(`relationships/${this.userId}`, {withCredentials: true}).then((res) => {
+                console.log("we nothing");
+            });
+        },
+        async blockFriends()
+        {
+            await this.$axios.$post(`relationships/${this.userId}`, {type: RelationshipType.blocked}, {withCredentials: true}).then((res) => {
+                console.log("i blocked him");
             });
         }
     }
