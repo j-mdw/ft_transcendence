fde21729895a524c35215e45eb8b144bd020540d
Florianne Coudert <coudert.florianne.69@gmail.com>
2021-12-22 22:08:27 +0100

    relationship work blocking included

--- front/pages/profile/friendsList.vue
+++ front/pages/profile/friendsList.vue
@@ -1,43 +1,38 @@
 <template>
-  <div>
+  <div class="mt-5">
     <v-list class="our_beige">
-      <div v-for="user in users" :key="user.title">
-        <div v-if="user.id != me.id">
-          
-          <v-list-item class="ml-n3">
-            
-            <v-badge
-              bottom
-              :color=colors[user.status]
-              offset-x="30"
-              offset-y="30"
-            >
-            <router-link :to="`/profile/${user.id}`">
-              <v-list-item-avatar class="mt-4 mb-4">
+      <div v-for="relationship in relationships" :key="relationship.id">
+        <div v-if="relationship.type == 3">
+            <v-list-item class="ml-n3">
+              <!-- <v-badge
+                bottom
+                :color="colors[user.status]"
+                offset-x="30"
+                offset-y="30"
+              > -->
+              <router-link :to="`/profile/${relationship.peerId}`">
+                <v-list-item-avatar class="mt-4 mb-4">
+                  <v-img
+                    :src="`http://localhost:4000/${relationship.peerId}`"
+                  />
                 
-                <v-img
-                  :src="`http://localhost:4000/${user.avatarPath}`"
-                ></v-img>
-               
-                <!-- </NuxtLink> -->
-              </v-list-item-avatar>
-               </router-link >
-            </v-badge>
-            <v-list-item-content>
-              <v-list-item-title v-text="user.pseudo" class="our_navy_blue--text" ></v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn v-ripple="false" plain  to="/channels">
-                  <messageLogo/>
-              </v-btn>
-            </v-list-item-action>
-            <v-list-item-action>
-              <v-btn v-ripple="false" plain  icon>
-                <pingpongLogo/>
-              </v-btn>
-            </v-list-item-action>
-          </v-list-item>
-          <v-divider></v-divider>
+                </v-list-item-avatar>
+              </router-link>
+              <!-- </v-badge> -->
+              <v-list-item-content>
+                <!-- <v-list-item-title class="our_navy_blue--text" v-text="user.pseudo" /> -->
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn v-ripple="false" plain to="/channels">
+                  <messageLogo />
+                </v-btn>
+              </v-list-item-action>
+              <v-list-item-action>
+                <v-btn v-ripple="false" plain icon>
+                  <pingpongLogo />
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
         </div>
       </div>
     </v-list>   
@@ -46,8 +41,8 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { User } from "~/models";
-import { usersStore, meStore } from "~/store";
+import { Relationship, User } from "~/models";
+import { usersStore, meStore, relationshipStore } from "~/store";
 import messageLogo from "../../components/Logo/messageLogo.vue";
 import pingpongLogo from "../../components/Logo/pingpongLogo.vue";
 
@@ -63,15 +58,15 @@ export default Vue.extend({
   },
 
   methods: {
-    getColor(): string {
-      return "";
-    },
   },
   computed: {
-    users(): User[] {
+    relationships(): Relationship[] {
+      return relationshipStore.all;
+    },
+    users (): User[] {
       return usersStore.allUsers;
     },
-    me(): User {
+    me (): User {
       return meStore.me;
     },
   },
